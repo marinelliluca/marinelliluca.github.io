57b414f721a69b8a6988417b3e8d14159dced81a
marinelliluca <marinelli.luca@proton.me>
2026-01-26 13:16:21 +0000

    Deploying to gh-pages from @ marinelliluca/marinelliluca.github.io@70d5ab744ed593e50377b9102ded7cd5dad95d98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,16 +23,11 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+        },{id: "news-invited-lecture-on-gender-coded-sound-computational-insights-into-toy-advertising-music-at-the-hochschule-für-musik-theater-und-medien-hannover-germany",
+          title: 'Invited lecture on “Gender‑Coded Sound: Computational Insights into Toy Advertising Music” at the...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-submitted-the-thesis-discussion-sometime-in-march",
+          title: 'I submitted the thesis! Discussion sometime in March.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
